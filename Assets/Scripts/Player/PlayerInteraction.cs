43620426ed970826945c8492d4f618d701edf482
alexandrelec <alexandre.leconte@telecom-sudparis.eu>
2025-06-07 12:12:31 +0200

    Coins

--- Assets/Scripts/Player/PlayerInteraction.cs
+++ Assets/Scripts/Player/PlayerInteraction.cs
@@ -1,16 +1,32 @@
+using System.Globalization;
+using System.Xml.Schema;
 using UnityEngine;
 
 public class PlayerInteraction : MonoBehaviour
 {
-    // Start is called once before the first execution of Update after the MonoBehaviour is created
-    void Start()
-    {
-        
-    }
+    [SerializeField] private TMPro.TextMeshProUGUI coinText;
+    private int numberOfCoins = 0;
+    [SerializeField] private int TotalCoins;
 
-    // Update is called once per frame
-    void Update()
+    private void Start()
     {
-        
+        // Initialize the coin text at the start of the game
+        coinText.text = "Coins : " + numberOfCoins.ToString() + "/" + TotalCoins.ToString();
+    }
+    public void OnTriggerEnter(Collider other)
+    {
+        if (other.CompareTag("Coin"))
+        {
+            
+            numberOfCoins++;
+            coinText.text = "Coins : " + numberOfCoins.ToString() + "/" + TotalCoins.ToString();
+            Destroy(other.gameObject);
+            if (numberOfCoins >= TotalCoins)
+            {
+                // Load the next level or show a victory message
+                Debug.Log("All coins collected! Level complete!");
+                // You can add your level completion logic here, like loading a new scene.
+            }
+        }
     }
 }
