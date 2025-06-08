33d09fc980a05bdd9821e8ab107eb7b09f42cc63
Tawbeee <119863395+Tawbeee@users.noreply.github.com>
2025-06-08 14:54:29 +0200

    Rajout sound (#2)
    * add ragdoll
    
    * add test button for ragdoll in inspector
    
    * add sounds
    
    * add events to soundbank
    
    * add states
    
    * add state on bg music
    
    * gen soundbank
    
    * add footsteps
    
    * update soundbank
    
    * add arrow
    
    * Object et transfo
    
    * add ragdoll
    
    * add test button for ragdoll in inspector
    
    * add sounds
    
    * add events to soundbank
    
    * add states
    
    * add state on bg music
    
    * gen soundbank
    
    * add footsteps
    
    * update soundbank
    
    * add arrow
    
    * update wwise project
    
    ---------
    
    Co-authored-by: alexandrelec <alexandre.leconte@telecom-sudparis.eu>

---
+++ Assets/Scripts/Player/RagdollEnabler.cs
@@ -0,0 +1,84 @@
+using UnityEngine;
+
+public class RagdollEnabler : MonoBehaviour
+{
+    [SerializeField]
+    private Animator Animator;
+    [SerializeField]
+    private Transform RagdollRoot;
+    [SerializeField]
+    private bool StartRagdoll = false;
+    
+    [Header("Inspector Controls")]
+    [SerializeField] private bool triggerRagdoll = false;
+    
+    // Only public for Ragdoll Runtime GUI for explosive force
+    public Rigidbody[] Rigidbodies;
+    private CharacterJoint[] Joints;
+    private Collider[] Colliders;
+
+    private void Awake()
+    {
+        Rigidbodies = RagdollRoot.GetComponentsInChildren<Rigidbody>();
+        Joints = RagdollRoot.GetComponentsInChildren<CharacterJoint>();
+        Colliders = RagdollRoot.GetComponentsInChildren<Collider>();
+
+        if (StartRagdoll)
+        {
+            EnableRagdoll();
+        }
+        else
+        {
+            EnableAnimator();
+        }
+    }
+
+    [ContextMenu("Enable Ragdoll")]
+    public void EnableRagdoll()
+    {
+        Animator.enabled = false;
+        foreach (CharacterJoint joint in Joints)
+        {
+            joint.enableCollision = true;
+        }
+        foreach (Collider collider in Colliders)
+        {
+            collider.enabled = true;
+        }
+        foreach (Rigidbody rigidbody in Rigidbodies)
+        {
+            rigidbody.linearVelocity = Vector3.zero;
+            rigidbody.detectCollisions = true;
+            rigidbody.useGravity = true;
+        }
+    }
+
+    [ContextMenu("Enable Animator")]
+    public void EnableAnimator()
+    {
+        Animator.enabled = true;
+        foreach (CharacterJoint joint in Joints)
+        {
+            joint.enableCollision = false;
+        }
+        foreach (Collider collider in Colliders)
+        {
+            collider.enabled = false;
+        }
+        foreach (Rigidbody rigidbody in Rigidbodies)
+        {
+            rigidbody.detectCollisions = false;
+            rigidbody.useGravity = false;
+        }
+    }
+    
+    // Update method to check for inspector trigger
+    private void Update()
+    {
+        if (triggerRagdoll)
+        {
+            triggerRagdoll = false;
+            EnableRagdoll();
+        }
+    }
+}
--- Assets/StreamingAssets/Audio/GeneratedSoundBanks/Wwise_IDs.h
+++ Assets/StreamingAssets/Audio/GeneratedSoundBanks/Wwise_IDs.h
@@ -13,10 +13,50 @@ namespace AK
 {
     namespace EVENTS
     {
+        static const AkUniqueID PLAY_ISKILLED = 3385860117U;
+        static const AkUniqueID PLAY_ISKILLEDBY_ARROW = 2095147940U;
+        static const AkUniqueID PLAY_ISKILLEDBY_FALL = 3446595322U;
+        static const AkUniqueID PLAY_ISKILLEDBY_LIGHTNING = 1734114553U;
+        static const AkUniqueID PLAY_ISKILLEDBY_SPIKE = 201740281U;
+        static const AkUniqueID PLAY_ISKILLEDBY_STOMP = 1365561248U;
+        static const AkUniqueID PLAY_ISKILLEDBY_WATER = 3013923882U;
         static const AkUniqueID PLAY_JAM_EVOLUTION = 3075399896U;
+        static const AkUniqueID PLAY_KILLINGOBJECT_ARROW = 2608619777U;
         static const AkUniqueID PLAY_KILLINGOBJECT_FIRE = 3990733286U;
+        static const AkUniqueID PLAY_KILLINGOBJECT_LIGHTNINGSTORM = 1457347807U;
+        static const AkUniqueID PLAY_KILLINGOBJECT_POISON = 3516034874U;
+        static const AkUniqueID PLAY_PLAYERSTATE = 1513779728U;
+        static const AkUniqueID PLAY_PLAYERSTATE_LIGHTNING_ACTIVATEBUTTON = 1856267421U;
+        static const AkUniqueID PLAY_PLAYERSTATE_SPIKE_DESTROY = 1795633096U;
+        static const AkUniqueID PLAY_RANDOM_FOOSTEPS = 1374893771U;
+        static const AkUniqueID STOP_KILLINGOBJECT_ARROW = 550981067U;
         static const AkUniqueID STOP_KILLINGOBJECT_FIRE = 2085787364U;
+        static const AkUniqueID STOP_KILLINGOBJECT_LIGHTNINGSTORM = 1769917345U;
+        static const AkUniqueID STOP_KILLINGOBJECT_POISON = 3063937136U;
     } // namespace EVENTS
+
+    namespace STATES
+    {
+        namespace PLAYERSTATE
+        {
+            static const AkUniqueID GROUP = 3285234865U;
+
+            namespace STATE
+            {
+                static const AkUniqueID ARROW = 1197258740U;
+                static const AkUniqueID FALL = 2512384458U;
+                static const AkUniqueID FIRE = 2678880713U;
+                static const AkUniqueID LIGHTNING = 3334464137U;
+                static const AkUniqueID NONE = 748895195U;
+                static const AkUniqueID NOTHING = 4248742144U;
+                static const AkUniqueID POISON = 1413156513U;
+                static const AkUniqueID SPIKE = 2812137U;
+                static const AkUniqueID STOMP = 1166633136U;
+                static const AkUniqueID WATER = 2654748154U;
+            } // namespace STATE
+        } // namespace PLAYERSTATE
+
+    } // namespace STATES
 
     namespace BANKS
     {
