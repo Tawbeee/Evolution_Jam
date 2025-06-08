adbf5e9f9e7f1eb96a09143cf425cd234c3092a7
Tawbe <tbennani091@gmail.com>
2025-06-08 13:31:02 +0200

    add events to soundbank

--- Assets/StreamingAssets/Audio/GeneratedSoundBanks/Wwise_IDs.h
+++ Assets/StreamingAssets/Audio/GeneratedSoundBanks/Wwise_IDs.h
@@ -13,8 +13,26 @@ namespace AK
 {
     namespace EVENTS
     {
+        static const AkUniqueID PLAY_ISKILLED = 3385860117U;
+        static const AkUniqueID PLAY_ISKILLEDBY_ARROW = 2095147940U;
+        static const AkUniqueID PLAY_ISKILLEDBY_FALL = 3446595322U;
+        static const AkUniqueID PLAY_ISKILLEDBY_LIGHTNING = 1734114553U;
+        static const AkUniqueID PLAY_ISKILLEDBY_SPIKE = 201740281U;
+        static const AkUniqueID PLAY_ISKILLEDBY_STOMP = 1365561248U;
+        static const AkUniqueID PLAY_ISKILLEDBY_WATER = 3013923882U;
         static const AkUniqueID PLAY_JAM_EVOLUTION = 3075399896U;
+        static const AkUniqueID PLAY_KILLINGOBJECT_ARROW = 2608619777U;
         static const AkUniqueID PLAY_KILLINGOBJECT_FIRE = 3990733286U;
+        static const AkUniqueID PLAY_KILLINGOBJECT_LIGHTNINGSTORM = 1457347807U;
+        static const AkUniqueID PLAY_KILLINGOBJECT_POISON = 3516034874U;
+        static const AkUniqueID PLAY_PLAYERSTATE_ARROW = 3553365610U;
+        static const AkUniqueID PLAY_PLAYERSTATE_FIRE = 2923772123U;
+        static const AkUniqueID PLAY_PLAYERSTATE_GRAVITY = 2709068703U;
+        static const AkUniqueID PLAY_PLAYERSTATE_LIGHTNING = 1056671487U;
+        static const AkUniqueID PLAY_PLAYERSTATE_LIGHTNING_ACTIVATEBUTTON = 1856267421U;
+        static const AkUniqueID PLAY_PLAYERSTATE_POISON = 742435475U;
+        static const AkUniqueID PLAY_PLAYERSTATE_SPIKE_DESTROY = 1795633096U;
+        static const AkUniqueID PLAY_PLAYERSTATE_WATER = 248667144U;
         static const AkUniqueID STOP_KILLINGOBJECT_FIRE = 2085787364U;
     } // namespace EVENTS
 
